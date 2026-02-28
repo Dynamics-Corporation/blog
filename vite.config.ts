3f24245785712206261bfbd0424737d1066d1010
Abel <malebochester@gmail.com>
2026-02-28 21:42:36 +0200

    update file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://cdn.jsdelivr.net/gh/Dynamics-Corporation/blog@main/dist/',
   plugins: [
     tailwindcss(),
     vue()
